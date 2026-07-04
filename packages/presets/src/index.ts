5106699681a734b209d0b97e90e2bae93a358530
Noinkin <87577662+Noinkin@users.noreply.github.com>
2026-07-04 18:56:30 +0100

    feat: export auto.js

--- packages/presets/src/index.ts
+++ packages/presets/src/index.ts
@@ -2,5 +2,6 @@ export * from './player.js';
 export * from './vehicle.js';
 export * from './command.js';
 export * from './kill.js';
+export * from './auto.js';
 
 export * from './types/index.js';
