32ac16540eb49d75a3daeeb4ef1eb6b175cf5e3c
devs_des1re <arjunbrij8811@gmail.com>
2026-07-04 18:11:24 +0100

    feat: auto announcement

--- packages/presets/src/auto.ts
+++ packages/presets/src/auto.ts
@@ -5,3 +5,9 @@ export function autoHint(client: Client, message: string, interval: number = 120
         client.commands.execute(`:hint ${message}`)
     }, interval)
 }
+
+export function autoAnnouncement(client: Client, message: string, interval: number = 12000) {
+    return setInterval(() => {
+        client.commands.execute(`:m ${message}`)
+    }, interval)
+}
