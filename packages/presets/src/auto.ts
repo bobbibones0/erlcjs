9c28624467527c1945510cb2456c75d71d9eb0b3
devs_des1re <arjunbrij8811@gmail.com>
2026-07-04 18:12:41 +0100

    fix: hint command

--- packages/presets/src/auto.ts
+++ packages/presets/src/auto.ts
@@ -2,7 +2,7 @@ import type { Client } from "@erlcjs/core";
 
 export function autoHint(client: Client, message: string, interval: number = 12000) {
     return setInterval(() => {
-        client.commands.execute(`:hint ${message}`)
+        client.commands.execute(`:h ${message}`)
     }, interval)
 }
 
